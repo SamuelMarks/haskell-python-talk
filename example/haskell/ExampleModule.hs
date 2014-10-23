c7b3b8596d1019cc5a3bc08c9f0d07c2ce2adbfd
Samuel Marks <samuelmarks@gmail.com>
2014-10-23 16:35:19 +1100

    Init

---
+++ example/haskell/ExampleModule.hs
@@ -0,0 +1,31 @@
+module ExampleModule where
+
+import Data.Char
+
+foo :: Double -> Double -> Double
+foo = (*)
+
+bar :: Int -> Int
+bar i = sum [1..i]
+
+baz :: Int -> Bool
+baz = (> 5)
+
+arr_arg :: [Int] -> Int
+arr_arg = sum
+
+arr_ret :: Int -> [Int]
+arr_ret i = [1..i]
+
+arr_complex :: [[Int]] -> [[Int]]
+arr_complex = map (map (* 2))
+
+string_fun :: String -> String
+string_fun str = str ++ reverse str
+
+char_test :: Char -> Int
+char_test = ord
+
+fibonacci :: Int -> Int
+fibonacci n = fibs !! n
+    where fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
---
+++ example/haskell/Export.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE ForeignFunctionInterface, TemplateHaskell #-}
+
+module Export where
+import Foreign.HaPy
+import ExampleModule
+
+initHaPy
+
+pythonExport 'foo
+pythonExport 'bar
+pythonExport 'baz
+
+pythonExport 'arr_arg
+pythonExport 'arr_ret
+pythonExport 'arr_complex
+
+pythonExport 'string_fun
+pythonExport 'char_test
+
+pythonExport 'fibonacci
